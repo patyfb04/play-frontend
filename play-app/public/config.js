87019fbbd8e360a94fca368c4c4f4094def9c7af
Patricia <patyfb04@gmail.com>
2026-03-04 22:06:22 -0500

    change urls

--- play-app/public/config.js
+++ play-app/public/config.js
@@ -1,21 +1,12 @@
-// This file will not end up inside the main application JavaScript bundle.
-// Instead, it will simply be copied inside the build folder.
-// The generated "index.html" will require it just before this main bundle.
-// You can thus use it to define some environment variables that will
-// be made available synchronously in all your JS modules under "src".
-//
-// Warning: this file will not be transpiled by Babel and cannot contain
-// any syntax that is not yet supported by your targeted browsers.
-
-window.CATALOG_SERVICE_URL = "http://play-catalog";
-window.CATALOG_ITEMS_API_URL = `${window.CATALOG_SERVICE_URL}/items`;
-
-window.INVENTORY_SERVICE_URL = "http://play-inventory";
-window.INVENTORY_ITEMS_API_URL = `${window.INVENTORY_SERVICE_URL}/items`;
-
-window.IDENTITY_SERVICE_URL = "http://play-identity";
+window.IDENTITY_SERVICE_URL = "/api/identity";
 window.USERS_API_URL = `${window.IDENTITY_SERVICE_URL}/users`;
 
-window.TRADING_SERVICE_URL = "http://play-trading";
+window.CATALOG_SERVICE_URL = "/api/catalog";
+window.CATALOG_ITEMS_API_URL = `${window.CATALOG_SERVICE_URL}/items`;
+
+window.INVENTORY_SERVICE_URL = "/api/inventory";
+window.INVENTORY_ITEMS_API_URL = `${window.INVENTORY_SERVICE_URL}/items`;
+
+window.TRADING_SERVICE_URL = "/api/trading";
 window.PURCHASE_API_URL = `${window.TRADING_SERVICE_URL}/purchase`;
 window.STORE_API_URL = `${window.TRADING_SERVICE_URL}/store`;
