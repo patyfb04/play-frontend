b353e9ae02bf9597ad32b1c9202703a333f03428
Patricia <patyfb04@gmail.com>
2026-03-06 23:20:18 -0500

    fix urls

--- play-app/public/config.js
+++ play-app/public/config.js
@@ -1,12 +1,12 @@
-window.IDENTITY_SERVICE_URL = "http://play-identity";
+window.IDENTITY_SERVICE_URL = "http://identity.play.com";
 window.USERS_API_URL = `${window.IDENTITY_SERVICE_URL}/users`;
 
-window.CATALOG_SERVICE_URL = "/api/catalog";
+window.CATALOG_SERVICE_URL = "http://catalog.play.com";
 window.CATALOG_ITEMS_API_URL = `${window.CATALOG_SERVICE_URL}/items`;
 
-window.INVENTORY_SERVICE_URL = "/api/inventory";
+window.INVENTORY_SERVICE_URL = "http://inventory.play.com";
 window.INVENTORY_ITEMS_API_URL = `${window.INVENTORY_SERVICE_URL}/items`;
 
-window.TRADING_SERVICE_URL = "/api/trading";
+window.TRADING_SERVICE_URL = "http://trading.play.com";
 window.PURCHASE_API_URL = `${window.TRADING_SERVICE_URL}/purchase`;
 window.STORE_API_URL = `${window.TRADING_SERVICE_URL}/store`;
