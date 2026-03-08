ad70748f1763e4f4b5efb24ffb4dc882d13331a2
Patricia <patyfb04@gmail.com>
2026-03-07 20:02:02 -0500

    fix urls

--- play-app/public/config.js
+++ play-app/public/config.js
@@ -1,16 +1,16 @@
 // Identity
-window.IDENTITY_SERVICE_URL = "http://4.174.167.15/identity";
+window.IDENTITY_SERVICE_URL = "https://4.174.167.15.nip.io/identity";
 window.USERS_API_URL = `${window.IDENTITY_SERVICE_URL}/users`;
 
 // Catalog
-window.CATALOG_SERVICE_URL = "http://4.174.167.15/catalog";
+window.CATALOG_SERVICE_URL = "https://4.174.167.15.nip.io/catalog";
 window.CATALOG_ITEMS_API_URL = `${window.CATALOG_SERVICE_URL}/items`;
 
 // Inventory
-window.INVENTORY_SERVICE_URL = "http://4.174.167.15/inventory";
+window.INVENTORY_SERVICE_URL = "https://4.174.167.15.nip.io/inventory";
 window.INVENTORY_ITEMS_API_URL = `${window.INVENTORY_SERVICE_URL}/items`;
 
 // Trading
-window.TRADING_SERVICE_URL = "http://4.174.167.15/trading";
+window.TRADING_SERVICE_URL = "https://4.174.167.15.nip.io/trading";
 window.PURCHASE_API_URL = `${window.TRADING_SERVICE_URL}/purchase`;
 window.STORE_API_URL = `${window.TRADING_SERVICE_URL}/store`;
